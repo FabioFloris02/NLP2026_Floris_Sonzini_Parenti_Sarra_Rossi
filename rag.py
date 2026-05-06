f708d6e61efaf1bcbcca006eeb4ec35bdfe0e071
AndreRossii <andrea.reds08@gmail.com>
2026-05-06 16:20:13 +0200

    Change name

--- rag.py
+++ rag.py
@@ -1,7 +1,7 @@
 # Import dataset from parquet
 from datasets import load_dataset
 
-def load_dataset(parquet_path):
+def load_parquet(parquet_path):
     ds = load_dataset("parquet", data_files=parquet_path, split="train")
 
     print(ds[0])
